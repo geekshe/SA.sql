ed6b136139dded5b9780e1abbe26d5f5a64ae53e
vagrant <vagrant@vagrant.vm>
2017-01-30 01:43:24 +0000

    Code complete up to 14

--- more-practice.sql
+++ more-practice.sql
@@ -3,15 +3,26 @@
 
 
 -- Insert a Brand
-
-
+INSERT INTO brands VALUES ('sub', 'Subaru', 1953, 'Tokyo, Japan', NULL);
 
 -- Insert Models
-
-
+INSERT INTO models (year, brand_id, name) VALUES (2015, 'sub', 'Outback');
+INSERT INTO models (year, brand_id, name) VALUES (2015, 'che', 'Malibu');
 
 -- Create an Awards Table
-
-
+CREATE TABLE awards(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    year INTEGER NOT NULL,
+    winner_id INTEGER REFERENCES models(model_id),
+    );
 
 -- Insert Awards
+INSERT INTO awards (name, year, winner_id)
+  VALUES ('IIHS Safety Award', '2015', 48);
+
+INSERT INTO awards (name, year, winner_id)
+  VALUES ('IIHS Safety Award', '2015', 47);
+
+INSERT INTO awards (name, year)
+  VALUES ('Best in Class', '2015');
